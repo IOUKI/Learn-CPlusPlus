019727dda52fbe21b7b48ca5402a3a1f03ce5a72
lan <quw112233456@gmail.com>
2024-01-12 05:16:12 +0800

    update

--- classes/files.cpp
+++ classes/files.cpp
@@ -4,11 +4,14 @@ using namespace std;
 
 int main() {
     // 創建或打開檔案
-    ofstream MyFile("filename.txt");
+    ofstream MyFile;
+    MyFile.open("D:/c++/Learn-CPlusPlus/Classes/filename.txt");
 
     // 寫入檔案
     MyFile << "檔案可能很棘手，但是他很有趣!";
 
     // 關閉檔案
     MyFile.close();
+
+    return 0;
 }
